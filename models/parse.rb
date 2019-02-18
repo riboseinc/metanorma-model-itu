16e9e8495011f047946ef4f06213c79c0a887ed9
Ronald Tse <ronald.tse@ribose.com>
2019-02-18 23:24:38 +0800

    Initial commit

---
+++ models/parse.rb
@@ -0,0 +1,24 @@
+class ItuClassDoc
+
+  def self.parse(file_content)
+
+  end
+
+end
+
+class ItuSerie
+
+  def self.parse(lines)
+
+end
+
+class ItuClass
+
+  attr_accessor :title, :code_start, :code_end, :code, :children, :serie
+
+  def self.parse(line)
+
+end
+
+
+"ITU-T D-Series Recommendations:"
